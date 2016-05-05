f990606dfc0dce356623eec5bd19a33f2955430d
mobileyangli <mobile_yangli@163.com>
2016-05-05 16:08:41 +0800

    加入wiki目录和src目录，添加项目初始化的代码

---
+++ src/V2EX-ObjC/V2EX-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  V2EX-ObjC
+//
+//  Created by 杨冬凌 on 16/5/5.
+//  Copyright © 2016年 Yang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/V2EX-ObjC/V2EX-ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  V2EX-ObjC
+//
+//  Created by 杨冬凌 on 16/5/5.
+//  Copyright © 2016年 Yang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
